f0b648820c9401bda43421c8a390666269fe8962
Jonas Duri <jonas.duri@mapudo.com>
2016-07-22 15:58:05 +0200

    Removed unused tasks and files

--- image-app/gulp/build.js
+++ image-app/gulp/build.js
@@ -91,4 +91,4 @@ gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/partials'), path.join(conf.paths.tmp, '/serve')]);
 });
 
-gulp.task('build', ['html', 'fonts', 'other','environment:production']);
+gulp.task('build', ['html', 'fonts', 'other']);
--- image-app/gulp/watch.js
+++ image-app/gulp/watch.js
@@ -10,7 +10,7 @@ function isOnlyChange(event) {
   return event.type === 'changed';
 }
 
-gulp.task('watch', ['scripts:watch', 'markups', 'inject','environment:development'], function () {
+gulp.task('watch', ['scripts:watch', 'markups', 'inject'], function () {
 
   gulp.watch([path.join(conf.paths.src, '/*.html'), 'bower.json'], ['inject-reload']);
 
